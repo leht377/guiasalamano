31083108696c10a5f5a3a64b28aa63df881f9b2a
EspinelB <despinel@unipacifico.edu.co>
2022-03-02 17:22:19 -0500

    vista de autenticado

--- view/noAutenticado.php
+++ view/noAutenticado.php
@@ -9,25 +9,27 @@
   <title>Autenticacion</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/ca7bc5fd05.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./assets/css/estilo.css">
 </head>
 
 <body>
-  <main id="autenti" class="d-flex justify-content-end  align-items-center container-fluid vh-100 ">
+  <main id="autenti" class=" d-flex justify-content-center  align-items-center container-fluid vh-100 ">
 
     <div style=" margin-right:50px; width: 500px; height: 500px;">
 
-      <div class="d-flex align-items-end justify-content-center " id="autenti2" style="height: 90%; width: 100%;">
-        <div class=" fs-5  mb-4" style="height: 110px; width: 260px;">
-          Por favor autentícate, dale clic en el botón siguiente:
+      <div class="ms-2 d-flex align-items-end justify-content-center " id="autenti2" style="height: 90%; width: 100%;">
+        <div class=" fs-5  mb-4  p-1  fw-bold" style=" height: 100px; width: 260px;">
+        Por favor autentícate, dale clic en el botón siguiente:
 
         </div>
 
       </div>
 
       <div class=" d-flex justify-content-center  mt-2" style="height: 10%; width: 100%;">
-        <a class="  btn btn-outline-primary " href="">
-          Iniciar sesion
+        <a class=" px-4 py-2 bg-primary text-white rounded " style ="background: rgb(2,0,36);
+        background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(2,97,154,0.8491771708683473) 13%, rgba(0,212,255,1) 100%);" href="index.php">
+        </i>   Iniciar sesion
         </a>
       </div>
     </div>
